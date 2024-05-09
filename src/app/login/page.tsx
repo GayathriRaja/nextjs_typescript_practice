2c4a8bf301cca376fb5d2311cef1aabf87f2d246
Gayathri Raja <gayathrraja@gmail.com>
2024-05-09 13:53:16 +0530

    first commit

---
+++ src/app/login/page.tsx
@@ -0,0 +1,20 @@
+// "use client"
+// import React from "react";
+// import Input from "next/input"
+
+
+//   export default function LoginPage() {
+//     return (
+//         <div>
+//             <Input type="text" placeholder="dummy@gmail.com"/>
+//         </div>
+//     )
+// }
+
+
+// import Link from 'next/link'
+ 
+// export default function Page() {
+//   return <Link href="/dashboard">Dashboard</Link>
+// }
+
